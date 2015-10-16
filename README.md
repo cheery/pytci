79289364f79b9de669308343a7a479b6fa8475da
Henri Tuhola <cheery@boxbase.org>
2015-10-16 17:07:37 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Compiler Infrastructure
+
+The Pytsi -initiative's goal is to provide a C compiler that is easy to set up, understand and maintain.
+
+So far it parses trigraphs.
+
+I programmed Pytsi to run on Python 2.7.10 because [PEP 0492](https://www.python.org/dev/peps/pep-0492/) is one big clusterfuck. The effort to complicate python compiler should be directed towards standardizing greenlets instead.
+
+## License
+
+MIT allows full interoperability and code sharing with other really interesting projects.
---
+++ trigraphs.py
@@ -0,0 +1,26 @@
+"""
+    The one of the most beloved features of C/C++.
+    https://en.wikipedia.org/wiki/Digraphs_and_trigraphs#C
+"""
+
+trigraphs = { "=":"#", "/":"\\", "(":"[", ")":"]", "!":"|", "<":"{", ">":"}", "-":"~" }
+
+def translate(sequence):
+    k = 0
+    for ch in sequence:
+        if ch == '?' and k < 2:
+            k += 1
+        elif ch == '?':
+            yield '?'
+        elif k == 2 and ch in trigraphs:
+            yield trigraphs[ch]
+            k = 0
+        else:
+            for _ in range(k):
+                yield '?'
+            yield ch
+            k = 0
+
+if __name__=='__main__':
+    test = "// Will the next line be executed????????????????/\na++;\n/??/\n* A comment *??/\n/"
+    print ''.join(translate(test))
